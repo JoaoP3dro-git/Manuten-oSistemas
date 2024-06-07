e75f267b87e7b97726c23114328572f7d6a9f121
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-06-07 10:03:50 -0300

    Organização cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,13 @@
+import style from './Card.module.css'
 /* eslint-disable react/prop-types */
 export const Card = (props) => {
   return(
-      <div>
+      <div className={style.productCard}>
           <h1>{props.name}</h1>
           <h2>{props.desc}</h2>
           <p>{props.value}</p>
           <img src={props.image} alt={props.name} width={150} height={"auto"}/>
+          <h3>{props.category}</h3>
       </div>
   )
 }
