3802d883ea4e741a6ca06f26e323a11bf4fd96fc
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-06-14 10:42:36 -0300

    a

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,23 @@ import { ApiCard } from './components/ApiCard'
 import produtos from './constants/produtos.json'
 import { api } from "./api/rmApi"
 import style from './App.module.css'
+import 'leaflet/dist/leaflet.css'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 function App() {
   const [show, setShow] = useState("")
   const [data, setData] = useState([])
   const [page, setPage] = useState("")
-
+  const position = [-25.42494104761025, -49.27224469379518]
 
   useEffect(() => {
     api.get(`/character/?page=${page}`).then((response) => {
-      if(!response.data.results){
+      if (!response.data.results) {
         console.log("Vazio")
       }
       setData(response.data.results)
     }).catch((error) => {
-      if(error.response.status === 404){
+      if (error.response.status === 404) {
         console.log("Esta pagina nao contem este personagem")
       }
       console.error(error)
@@ -27,52 +29,63 @@ function App() {
 
   return (
     <>
-    <div className={style.wrapBtns}>
-      <button onClick={() => setShow("prod")}>Produtos</button>
-      <button onClick={() => setShow("api")}>API</button>
-      <button onClick={() => setShow("map")}>Mapa</button>
-    </div>
-    <div className={style.wrapPage}>
-      <h1>Exercícios de manutenção</h1>
-     {show === "prod" &&
-        <>
-          <h2>Showroom de produtos</h2>
+      <div className={style.wrapBtns}>
+        <button onClick={() => setShow("prod")}>Produtos</button>
+        <button onClick={() => setShow("api")}>API</button>
+        <button onClick={() => setShow("map")}>Mapa</button>
+      </div>
+      <div className={style.wrapPage}>
+        <h1>Exercícios de manutenção</h1>
+        {show === "prod" &&
+          <>
+            <h2>Showroom de produtos</h2>
             <div className={style.wrapCards}>
-            {produtos.map((item) => {
-              return(
-                <Card {...item}/>
-              )
-             })}
+              {produtos.map((item) => {
+                return (
+                  <Card {...item} />
+                )
+              })}
             </div>
-        </>
-      }
-     {show === "api" &&
-        <>
-          <h2>Rick and Morty API</h2>
+          </>
+        }
+        {show === "api" &&
+          <>
+            <h2>Rick and Morty API</h2>
             <div>
-               <input type="text" placeholder="1/43" value={page} onChange={(event) => setPage(event.target.value)}/>
+              <input type="text" placeholder="1/43" value={page} onChange={(event) => setPage(event.target.value)} />
+              <input type="text" placeholder="Name"/>
             </div>
             <div className={style.wrapCards}>
-            {data.map((item) => { 
-             return(
-              <div key={item.id}>
-                <ApiCard {...item}/>
-                {/* <button onClick={() => {}}>Info</button> */}
-              </div>
-              )
-           })}
+              {data.map((item) => {
+                return (
+                  <div key={item.id}>
+                    <ApiCard {...item} />
+                    {/* <button onClick={() => {}}>Info</button> */}
+                  </div>
+                )
+              })}
             </div>
-       </>
-      }
-     {show === "map" &&
-        <>
-      <h2>Mapa</h2>
-          <div>
-              mapa aqui
-          </div>
-         </>
-      }
-    </div>
+          </>
+        }
+        {show === "map" &&
+          <>
+            <h2>Mapa</h2>
+            <div>
+              <MapContainer style={{ height: 500, width: 500 }} center={position} zoom={12}>
+                <TileLayer
+                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+
+                <Marker position={position}>
+                  <Popup>
+                    Senai
+                  </Popup>
+                </Marker>
+              </MapContainer>
+            </div>
+          </>
+        }
+      </div>
     </>
   )
 }
